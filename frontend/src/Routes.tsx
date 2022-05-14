936d9e25b4320f6481d48521f2916a8232383830
DjairCosta <riajd10@gmail.com>
2022-05-14 17:06:48 -0300

    Finish pagination

---
+++ frontend/src/Routes.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Catalog from './pages/Catalog';
+import Admin from './pages/Admin';
+import Navbar from './core/components/Navbar';
+import ProductDetails from '../src/pages/Catalog/components/ProductDetails'
+
+
+const Routes = () => (
+    <BrowserRouter>
+      <Navbar />
+        <Switch>
+            <Route  path="/" exact>
+                <Home />
+            </Route>
+            <Route  path="/products" exact>
+                <Catalog />
+            </Route>
+            <Route  path="/products/:productId" exact>
+                <ProductDetails />
+            </Route>
+            <Route  path="/admin">
+                <Admin />
+            </Route>  
+        </Switch>
+    </BrowserRouter>
+
+);
+
+export default Routes;
